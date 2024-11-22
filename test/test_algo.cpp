5e2c7dd595e4875344ddd02d3945dfff4da71de8
CalmelsAmaury <amaury9744@gmail.com>
2024-11-22 19:29:44 +0100

    dev

--- test/test_algo.cpp
+++ test/test_algo.cpp
@@ -6,5 +6,17 @@ int add(int a, int b);
 int main() {
     assert(add(2, 3) == 5);
     assert(add(-1, 1) == 0);
+
+
+    Model chess;
+    Presentation pres;
+    ClassAlgorithm algo;
+    chess.load("Chess.cfg");
+    Move w=algo.nextMove(chess, Player::White);
+    assert(w==Move("tw1:h1"));
+    pres.show(chess);
+    Move b=algo.nextMove(chess, Player::Black);
+    pres.show(chess);
+    assert(w==Move("tb1:h1"));
     return 0;
 }
