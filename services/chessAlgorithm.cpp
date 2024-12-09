771a0ee77a004708648b0519fde4fae89d31cc84
Fabrice Lainard Programming <LainardFabriceProgramming@outlook.com>
2024-12-09 12:13:44 +0100

    Update chessAlgorithm.cpp

--- services/chessAlgorithm.cpp
+++ services/chessAlgorithm.cpp
@@ -3,13 +3,13 @@
 
 class ChessAlgorithm{
     public:
-        Board nextMove(Model& model )
+        BoardPtr nextMove(Model& model )
         {
            auto board = model.CreateBoard();
             for(int row =0; row< 8; ++row) 
                    for(int col =0; col< 8; ++col) 
             {   
-                BoardPositions& bpos = board[row][col];
+                BoardPositions& bpos = (*board)[row][col];
                 if(!bpos.piece) continue;
                bpos.possiblePositions = bpos.piece->nextPossibleMoves();
                 
